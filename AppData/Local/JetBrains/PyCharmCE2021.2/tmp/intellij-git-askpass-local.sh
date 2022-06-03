cf6d91ed1b23582bf589eeb4d0771f53ee29bd4e
utkir-2001 <utkir.djumayev@gmail.com>
2022-06-04 00:45:27 +0500

    first commit

---
+++ AppData/Local/JetBrains/PyCharmCE2021.2/tmp/intellij-git-askpass-local.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+"C:/Program Files/JetBrains/PyCharm Community Edition 2021.2.3/jbr/bin/java" -cp "C:/Program Files/JetBrains/PyCharm Community Edition 2021.2.3/plugins/git4idea/lib/git4idea-rt.jar;C:/Program Files/JetBrains/PyCharm Community Edition 2021.2.3/lib/3rd-party.jar" git4idea.http.GitAskPassApp "$@"
